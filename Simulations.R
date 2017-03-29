6abee295c76fba10f4f91fcc354e38113d141ae8
Maria Cuellar Correa <mcuellar@phd-serv4.heinz.cmu.edu>
2017-03-29 19:16:34 -0400

    Fixed simulations, I think.

--- Simulations.R
+++ Simulations.R
@@ -12,9 +12,8 @@ WD_thesis = "/home/mcuellar/Thesis"
 library(clusterPower) # lets me use expit function
 library(dplyr) # lets me use sample_n function
 library(hydroGOF) # lets me use rmse function
-library(np) # lets me use nonparametric models (https://cran.r-project.org/web/packages/np/np.pdf)
-library(beepr)
-
+library(np) # lets me use nonparametric models, like kernel (https://cran.r-project.org/web/packages/np/np.pdf)
+library(randomForest) # lets me do random forests
 
 # Function to simulate estimation of PC (gamma) as plugin, nonparametric, parametric IF, and nonparametric IF
 
@@ -24,10 +23,10 @@ fun.simulate = function(samplesize){
   #set.seed(400)
   
   # true parameters
-  samplesize = 1000
+  #samplesize = 1000
   index = 1:samplesize
   beta = 0.5
-  Y1 = rbinom(n = samplesize, size = 1, prob = beta)
+  Y1 = rbinom(n = samplesize, size = 1, prob = beta) #rbinom(n = samplesize, size = 1, prob = expit(210 + 27.4*X1 + 13.7*X2 + 13.7*X3 + 13.7*X4)) 
   
   X1 = rnorm(n = samplesize, mean = 0, sd = 1) # correct model
   X2 = rnorm(n = samplesize, mean = 0, sd = 1)
@@ -60,120 +59,119 @@ fun.simulate = function(samplesize){
   df_wy0$Y = ifelse(df_wy0$A==1, df_wy0$Y1, df_wy0$Y0) 
   
   # ordering data so it's as it was at the beginning
-  df_gendat = df_wy0[order(df_wy0$index),] 
-  head(df_gendat)
+  dff = df_wy0[order(df_wy0$index),] 
+  head(dff)
   
   
   
   # now getting into the models ---
   
   # Correctly specified model: parametric plug-in (P_PI) estimator:
-  corr_P_PI = glm(Y~X1+X2+X3+X4, data = df_gendat, family = "binomial")
+  cor_P_PI_mu0 = glm(Y~X1+X2+X3+X4, data = dff[which(dff$A==0),], family = "binomial") #fit only on items with A=0
+  cor_P_PI_mu1 = glm(Y~X1+X2+X3+X4, data = dff[which(dff$A==1),], family = "binomial") #fit only on items with A=1
   
-  ######### Crap, how do I get gamma hat?? ##########
+  # getting fitted values (after inverse link function)
+  cor_P_PI_mu0_hat = expit(predict.glm(cor_P_PI_mu0, newdata = dff))
+  cor_P_PI_mu1_hat = expit(predict.glm(cor_P_PI_mu1, newdata = dff))
   
-  df_gendat$fittedY = fitted(corr_P_PI)
+  # calculating gamma hat
+  cor_P_PI_gammahat = (cor_P_PI_mu1_hat - cor_P_PI_mu0_hat)/cor_P_PI_mu1_hat
   
-  df_gendat_Y11 = df_gendat[which(df_gendat$Y1==1),]
-  
-  
-  
-  gammahat = fitted(corr_P_PI)
-  RMSE_P_PI = sqrt(  mean( (gammahat - gamma)^2 )  )
-  
-  
-  # gettign fitted values (after inverse link function)
-  fit_P_PI = fitted(corr_P_PI)
+  # RMSE 
+  RMSE_cor_P_PI = sqrt(  mean( (cor_P_PI_gammahat - gamma)^2 )  )
+
 
   
-  # making them the same length
-  cut_fit_P_PI_m0 = fit_P_PI_m0[1:(min(length(fit_P_PI_m0), length(fit_P_PI_m1)))]
-  cut_fit_P_PI_m1 = fit_P_PI_m1[1:(min(length(fit_P_PI_m0), length(fit_P_PI_m1)))]
+  # Misspecified model: parametric plug-in (P_PI) estimator:
+  mis_P_PI_mu0 = glm(Y~X1star+X2star+X3star+X4star, data = dff[which(dff$A==0),], family = "binomial") #fit only on items with A=0
+  mis_P_PI_mu1 = glm(Y~X1star+X2star+X3star+X4star, data = dff[which(dff$A==1),], family = "binomial") #fit only on items with A=1
   
-  # estimating gamma hat
-  cutgammahat_P_PI = 1-cut_fit_P_PI_m0/cut_fit_P_PI_m1
+  # getting fitted values (after inverse link function)
+  mis_P_PI_mu0_hat = expit(predict.glm(mis_P_PI_mu0, newdata = dff))
+  mis_P_PI_mu1_hat = expit(predict.glm(mis_P_PI_mu1, newdata = dff))
+
+  # calculating gamma hat
+  mis_P_PI_gammahat = (mis_P_PI_mu1_hat - mis_P_PI_mu0_hat)/mis_P_PI_mu1_hat
   
-  # MSE 
-  cutgamma = gamma[1:(length(cutgammahat_P_PI))]
-  MSE_P_PI = mean(  (cutgammahat_P_PI - cutgamma)^2  )
+  # RMSE 
+  RMSE_mis_P_PI = sqrt(  mean( (mis_P_PI_gammahat - gamma)^2 )  )
   
-  
-  
-  # Nonparametric plug-in (N_PI) estimator
-  N_PI_m0 = npregbw(Y~X, bandwidth.compute = TRUE, bwmethod = "cv.aic", data = subset(df_all, A == 0))
-  N_PI_m1 = npregbw(Y~X, bandwidth.compute = TRUE, bwmethod = "cv.aic", data = subset(df_all, A == 1))
-  
-  # gettign fitted values (after inverse link function)
-  fit_N_PI_m0 = predict(N_PI_m0)$mean
-  fit_N_PI_m1 = predict(N_PI_m1)$mean
-  
-  # making them the same length
-  cut_fit_N_PI_m0 = fit_N_PI_m0[1:(min(length(fit_N_PI_m0), length(fit_N_PI_m1)))]
-  cut_fit_N_PI_m1 = fit_N_PI_m1[1:(min(length(fit_N_PI_m0), length(fit_N_PI_m1)))]
-  
-  # estimating gamma hat
-  cutgammahat_N_PI = 1-cut_fit_N_PI_m0/cut_fit_N_PI_m1
-  
-  # MSE 
-  cutgamma = gamma[1:(length(cutgammahat_N_PI))]
-  MSE_N_PI = mean(  (cutgammahat_N_PI - cutgamma)^2  )
+
   
   
-  return(c(MSE_P_PI, MSE_N_PI))
+  # Correctly specified model: NONparametric plug-in (P_PI) estimator:
+  cor_N_PI_mu0 = randomForest(factor(Y)~X1+X2+X3+X4, data = dff[which(dff$A==0),])
+  cor_N_PI_mu1 = randomForest(factor(Y)~X1+X2+X3+X4, data = dff[which(dff$A==1),])
+  
+  # getting fitted values (after inverse link function)
+  cor_N_PI_mu0_hat = as.numeric(predict(cor_N_PI_mu0, newdata=dff))
+  cor_N_PI_mu1_hat = as.numeric(predict(cor_N_PI_mu1, newdata=dff))
+  
+  # calculating gamma hat
+  cor_N_PI_gammahat = (cor_N_PI_mu1_hat - cor_N_PI_mu0_hat)/cor_N_PI_mu1_hat
+  
+  # RMSE 
+  RMSE_cor_N_PI = sqrt(  mean( (cor_N_PI_gammahat - gamma)^2 )  )
+  
+  
+  
+  # Misspecified model: NONparametric plug-in (P_PI) estimator:
+  mis_N_PI_mu0 = randomForest(factor(Y)~X1star+X2star+X3star+X4star, data = dff[which(dff$A==0),])
+  mis_N_PI_mu1 = randomForest(factor(Y)~X1star+X2star+X3star+X4star, data = dff[which(dff$A==1),])
+  
+  # getting fitted values (after inverse link function)
+  mis_N_PI_mu0_hat = as.numeric(predict(cor_N_PI_mu0, newdata=dff))
+  mis_N_PI_mu1_hat = as.numeric(predict(cor_N_PI_mu1, newdata=dff))
+  
+  # calculating gamma hat
+  mis_N_PI_gammahat = (mis_N_PI_mu1_hat - mis_N_PI_mu0_hat)/mis_N_PI_mu1_hat
+  
+  # RMSE 
+  RMSE_mis_N_PI = sqrt(  mean( (mis_N_PI_gammahat - gamma)^2 )  )
+  
+
+  
+  # Function will return this
+  return(c(RMSE_cor_P_PI, RMSE_mis_P_PI, RMSE_cor_N_PI, RMSE_mis_N_PI))
 }
 
+fun.simulate(1000)
 
 # repeat simulation for different n's
 #samplesizes = c(500, 1000, 1500, 2000, 2500, 3000, 3500, 10000)
-samplesizes = c(1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000,
-                1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500,
-                2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000,
-                2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500,
-                3000, 3000, 3000, 3000, 3000, 3000, 3000, 3000, 3000, 3000,
-                3500, 3500, 3500, 3500, 3500, 3500, 3500, 3500, 3500, 3500,
-                4000, 4000, 4000, 4000, 4000, 4000, 4000, 4000, 4000, 4000,
-                4500, 4500, 4500, 4500, 4500, 4500, 4500, 4500, 4500, 4500,
-                5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000,
-                10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000)
+samplesizes = c(500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 
+                1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000,
+                1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500)
 
-  # Making the list of samplesizes
-  # ss = data.frame()
-  # ss1 = data.frame()
-  # theseq = seq(from = 1000,to = 5000,by = 500)
-  # for(i in 1:length(theseq)){
-  #   ss1 = rep((theseq)[i], times=10)
-  #   ss = c(ss, ss1)
-  # }
-  # length(ss)
-  # 
-  # for(ii in 91:100){
-  #   ss[[ii]] = 10000
-  # }
-  # sss=as.vector(unlist(ss))
-  
-  
-samplesizestot = seq(from = 1000,to = 5000,by = 500)
+samplesizestot = seq(from = 500,to = 1500,by = 500)
 
-arr <- array(dim=c(length(samplesizes),3))
-colnames(arr) <- c("sample_sizes", "P_PI_simulations", "N_PI_simulations")
+arr <- array(dim=c(length(samplesizes),5))
+colnames(arr) <- c("sample_sizes", "cor_P_PI_RMSE", "mis_P_PI_RMSE", "cor_N_PI_RMSE", "mis_N_PI_RMSE")
 arr[1:length(samplesizes),1] = samplesizes
 arr
 
 for(s in 1:length(samplesizes)){
   arr[s, 2] = fun.simulate(samplesizes[s])[1]
   arr[s, 3] = fun.simulate(samplesizes[s])[2]
+  arr[s, 4] = fun.simulate(samplesizes[s])[3]
+  arr[s, 5] = fun.simulate(samplesizes[s])[4]
 }
 
-df.sim1 = as.data.frame(arr)
-df.sim1
-
-df.sim.fin = rbind(df.sim, df.sim1)
-df.sim.fin
-
-df.sim = df.sim.fin
+df.sim = as.data.frame(arr)
+df.sim
 
 setwd(WD_thesis)
-save(df.sim.fin, file="df.sim.fin.rda")
+save(df.sim, file="df.sim.fin-2017-03-29.rda")
+
+# Making histograms
+dat_n500 = df.sim[which(df.sim$sample_sizes==500),]
+
+# How to make these??
+mean(dat_n500$cor_P_PI_RMSE)
+mean(dat_n500$mis_P_PI_RMSE)
+
+mean(dat_n500$cor_N_PI_RMSE)
+mean(dat_n500$mis_N_PI_RMSE)
 
 
 # Scatterplot
@@ -181,7 +179,7 @@ setwd(WD_thesis)
 pdf("MSE_estimating_PC_samesamplesize1.pdf", width=8, height=6)
 par(mfrow=c(1,1))
 plot(df.sim$sample_sizes, df.sim$P_PI_simulations, col="blue", lwd=2, ylim=c(0,0.06), 
-     main=" Squared Error (MSE) of PC Estimation", ylab="Mean Squared Error (MSE)", xlab="Sample size")
+     main="Root Mean Squared Error (RMSE) of PC Estimation", ylab="Mean Squared Error (MSE)", xlab="Sample size")
 points(df.sim$sample_sizes, df.sim$N_PI_simulations, col="red", lwd=2)
 panel.first = grid(lwd=1, col="gray30")
 legend(x = 6000, y = 0.01, cbind("Parametric PI estimates", "Nonparametric PI estimates"), cex=0.8, lwd=2, col=c("blue", "red"),bg = "white")
